4cced5227a7d6990e6bad24ebeaadb059120404f
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-12 12:36:41 +0800

    Added Scanner and Creating Object

--- src/Arraylist/Main.java
+++ src/Arraylist/Main.java
@@ -3,15 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package arraylist;
+
 import java.util.Scanner;
+
 /**
  *
- 
+ * @author HP
  */
 public class Main {
     
     public static void main(String[] args) {
        
+        Scanner sc = new Scanner(System.in);
+        //Creating object of the class to access all the methods on the Item Manager
+        ItemManager list = new ItemManager();
+        
+        //Inializing variables 
+        String item, newItem, finditem;
+        int index;
     }
 }
+                
