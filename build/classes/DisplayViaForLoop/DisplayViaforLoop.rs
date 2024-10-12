6dc6ca33a9398766ff9f4d93e4c1d62dafe9cfcd
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-12 10:20:35 +0800

    Added DisplayViaForLoop

---
+++ build/classes/DisplayViaForLoop/DisplayViaforLoop.rs
@@ -0,0 +1,2 @@
+DisplayViaforLoop.DisplayViaforLoop
+DisplayViaForLoop.DisplayViaForLoop
---
+++ src/DisplayViaForLoop/DisplayViaforLoop.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DisplayViaforLoop;
+/**
+ *
+ * @author Joshua
+ */
+public class DisplayViaforLoop {
+     public static void main(String[] args) {
+        
+        //Arrays 
+        String[] names = {"Oscar", "Wawang", "Kakang"};
+        String[] year = {"First year", "Second Year", "Third year"};
+        
+        //Foor Loops to Print All the values of the variable year and names
+        for(int i = 0; i < year.length; i++) {
+             //Output
+             System.out.println(names[i] + " " + year[i]);
+         }
+         
+     }
+}
