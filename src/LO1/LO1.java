32da84391c7c6973190332225992778ec8bb3a17
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-13 19:56:38 +0800

    Added Array and Output

---
+++ src/LO1/LO1.java
@@ -0,0 +1,23 @@
+package LO1;
+/**
+ *
+
+ */
+public class LO1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Array 
+        int[] num = {1, 2, 3};
+        
+        // Output to print the values of the Array individually
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        
+        System.out.println("~~~~~~~");
+        
+    }
+}
