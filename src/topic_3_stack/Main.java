d65c7d6bde60017f7ead5b63470ae46be3c7e407
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-14 00:04:40 +0800

    Added a new package named topic_3_stack and a class named Main and Stack

---
+++ src/topic_3_stack/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_3_stack;
+
+/**
+ *
+ * @author hi
+ */
+public class Main {
+    
+}
