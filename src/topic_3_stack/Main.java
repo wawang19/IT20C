06c1b8e388a427200e707920eee9515f5fea7ae5
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-14 00:23:11 +0800

    Added case 1 to display the size of the stack and added size method

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -6,10 +6,33 @@
 
 package topic_3_stack;
 
+import java.util.Scanner;
 /**
  *
- * @author hi
+ * @author HP
  */
 public class Main {
-    
+   public static void main(String[] args) {
+       
+       Scanner sc = new Scanner(System.in);
+       
+       Stack stack = new Stack(5);
+
+       System.out.println("Hello there!");
+       
+       while(true) {
+           System.out.println("Choose what you want to do: "
+                    + " 1. Size"
+                    + " 2. Push"
+                    + " 3. Pop"
+                   +  " 4. Exit");
+            int choice = sc.nextInt();
+        switch(choice) {
+            case 1:
+                      System.out.println("Stack size is: " + stack.size());
+                      System.out.println("~~~~~~~~~~~~~~~~~~");
+                      break;
+        }
+       }
+   }
 }
--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -8,8 +8,29 @@ package topic_3_stack;
 
 /**
  *
- * @author hi
+ * @author HP
  */
 public class Stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+    
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+    }
+    
+     public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+    
+    public int size() {
+        return top + 1;
+    }
     
 }
