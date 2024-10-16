de56789e2fe17ce42aeceb378654d872eb05e172
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-16 14:08:36 +0800

    Added Initial Stack-calculator

---
+++ src/topic_3_stack_calculator/Calculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_3_stack_calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Stack;
+
+public class Calculator extends JFrame implements ActionListener {
+    private Stack<Integer> stack = new Stack<>();
+    private JTextArea display;
+
+    public Calculator() {
+        setTitle("Stack Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(10, 10));
+
+    }
+}
