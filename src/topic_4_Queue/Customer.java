103a33359f1e8705bedce211569bfbe0f313f3d0
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-17 22:17:29 +0800

    Added contents inside the Customer ang Queue

--- src/topic_4_Queue/Customer.java
+++ src/topic_4_Queue/Customer.java
@@ -3,12 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package topic_4_Queue;
+package topic_4_queue;
 
 /**
  *
- 
  */
 public class Customer {
     
+    private final String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+    
+
+    
