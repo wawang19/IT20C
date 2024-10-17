e34acd3d1b858afc3a9c11b8602459fa5d3e028a
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-17 22:02:16 +0800

    Added choices

--- src/topic_4_Queue/Main.java
+++ src/topic_4_Queue/Main.java
@@ -3,11 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package topic_4_Queue;
+package topic_4_queue;
 
 /**
  *
+ * @author ITLAB2-PC14-pagongpong
  */
+import java.util.Scanner;
 public class Main {
+
     
+    public static void main(String[] args) {
+     	
+		
+		 Queue customerQueue = new Queue();
+                 
+		 Scanner scan = new Scanner(System.in);
+                 
+                 
+                 
+                 System.out.println("Welcome to my queue system with scanner UI");
+                 
+                 while(true){
+                 System.out.println("");
+                 System.out.println("What do you want to do in the queue?");
+                 System.out.println("1. Enqueue");
+                 System.out.println("2. Dequeue");
+                 System.out.println("3. Display");
+                 System.out.println("4. Leave");
+                 
+                 }
+    }
 }
